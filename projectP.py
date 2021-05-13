2884523ce354e7f409930a22ee864db7c6879dca
ccotton-dev <78053919+ccotton-dev@users.noreply.github.com>
2021-05-12 19:59:53 -0700

    push pseudo code

--- projectP.py
+++ projectP.py
@@ -1,18 +1,17 @@
-"""Plain English
-Define main function
-establish an open list to store the first set of names that corresponds to the president
-open the text file that contains the presidents' names
-read from the file and strip everything but the characters needed and assign each presidents 
-name to a place on the list
-establish an open list to store the names of all vice presidents
-open the second text file that contains all vice presidents' names 
-read into the file and strip it of all content except the characters we need
-assign each string of names to a value inside the list 'vp'
+"""Pseudo Code
+define main
+pres = []
+open presiden.txt file to read
+pres = line.strip() for each line in the file
+vp = []
+open vps.txt file to read
+vp = line.strip() for each line in the text file
 
-take input from the user and prompt them with the question of naming a current or former president
-iterate through a for loop for the length of the list with presidents' names
-if the value in the list is equivalent to what the user input, print out the corresponding
-name of the vice president that lies in the same position for its separate text file
+president = input from user
+prompt user with 'Name a current or former president (first and last name with capitalization):'
+for i in range(len(pres)):
+    if(president == pres[i]):
+        print(vp[i])
 
-run the main() function
+main()
 """
